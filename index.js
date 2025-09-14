b731fede1808159b90241c5b994f6f03e2b3ad96
Collins Kibet <kollcibe05@gmail.com>
2025-09-14 09:15:02 +0300

    find method array

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+/*
+function superbowlWin(record) {
+    if (["year"]["result"] === "W") {
+    console.log( ["year"]["result"])
+    
+  }
+}
+record.find(superbowlWin)
+*/
+
+function superbowlWin(records) {  
+  let winning = records.find(entry => entry.result === "W");
+
+  if (winning) {
+    return winning.year; // Assuming 'year' is a direct property of each object
+  }
+  return undefined;
+}
